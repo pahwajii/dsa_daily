c5fb8dbcf73c356a9124b967f3c924416ad22b38
pahwajii <127213957+pahwajii@users.noreply.github.com>
2026-01-06 02:33:45 +0530

    lc_1390

---
+++ lc_1390.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int sum_total = 0;
+
+        for (int num : nums) {
+            int count = 0;
+            int sum = 0;
+
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int d1 = i;
+                    int d2 = num / i;
+
+                    if (d1 == d2) {
+                        count++;
+                        sum += d1;
+                    } else {
+                        count += 2;
+                        sum += d1 + d2;
+                    }
+
+                    if (count > 4) break;
+                }
+            }
+
+            if (count == 4) {
+                sum_total += sum;
+            }
+        }
+
+        return sum_total;
+    }
+};
